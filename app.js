ab3e9be6346789f48e4bb58dce30de03aedc2ccc
emre <emre>
2024-03-17 13:51:07 +0300

    String metotlari

---
+++ app.js
@@ -0,0 +1,73 @@
+// String Sınıf Metotları
+
+let kurs  ="Modern Web Geliştirme Kursu";
+let tarih = "2024";
+//charAt()
+//concat()
+//indexOf()
+//lastIndexOf()
+//toUpperCase()
+//toLowerCase()
+//trim()
+//slice()
+//substring()
+//replace()
+//split()
+//valueOf()
+//startsWift()
+//endsWift()
+
+//CHARAT METOT KULLANIMI : index numarasına göre içindeki karakteri çağırır
+// let karakter = kurs.charAt(1);
+// console.log(karakter);
+
+//CONCAT : string değerleri birleştirmek için kullanılır.
+// let sonuc = kurs.concat(tarih);
+// console.log(sonuc);
+
+//İNDEXOF : karaktere karşı gelen index numarasını çağırır
+// let index = kurs.indexOf("o");
+// console.log(index);
+
+//LASTINDEX 
+// let lastIndexOf = kurs.lastIndexOf("Kursu");
+// console.log(lastIndexOf);
+
+//TOUPPERCASE : karakterleri büyük yapar
+// let sonuc = kurs.toUpperCase();
+// console.log(sonuc);
+
+//TOLOWERCASE : karakterleri küçük yapar
+// let sonuc= kurs.toLowerCase();
+// console.log(sonuc);
+
+//TRİM : space (boşlukları) temizler
+// console.log(kurs.trim());
+
+//SLİCE : dilimleme yapar
+// console.log(kurs);
+// console.log(kurs.slice(22,27));
+
+//SUBSTRİNG : DİLİMLEME YAPAR
+// console.log(kurs);
+// console.log(kurs.slice(0,6));
+
+//REPLACE : içindeki değer yada kelimeyi değiştirme metodu
+// console.log(kurs);
+// console.log(kurs.replace("Web","Arayüz"));
+
+//SPLİT : dizi haline getirir
+// console.log(kurs);
+// let dizi = kurs.split(" ");
+// console.log(dizi);
+
+//VALUEOF :
+// console.log(kurs.valueOf());
+
+//STARTSWİFT : başlangıç karakterini veya kelimeyi sorgular (true or false)
+// console.log(kurs);
+// console.log(kurs.startsWith("M"));
+
+//ENDSWİFT : son karakterini veya kelimeyi sorgular (true or false)
+// console.log(kurs);
+// console.log(kurs.endsWith("Kursu"));
